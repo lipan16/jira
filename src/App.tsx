25fab2a5a893857af6aeccc26e2a053647873eca
lipan16 <lipan16@lzu.edu.cn>
2021-07-09 15:34:57 +0800

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Temperature from "./components/temperature";
+
+
+function App() {
+    return (
+        <Temperature/>
+    );
+}
+
+export default App;
