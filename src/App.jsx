eef3c01d435fc6070a6209f3687da41ee38a7d0f
lipan16 <lipan16@lzu.edu.cn>
2021-07-13 22:06:59 +0800

    修改部分配置，引入zarm

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import routes from './router';
 import Home from './pages/Home';
 
+import {ConfigProvider} from 'zarm';
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
+import 'zarm/dist/zarm.css';
+
 function App(){
     return (
-        <BrowserRouter>
-            <Home/>
-            <Switch>
-                {
-                    routes.map(route =>
-                        // 使用exact来实现精准匹配
-                        <Route exact key={route.path} path={route.path}>
-                            <route.component/>
-                        </Route>
-                    )
-                    // <Route component={NotFound}></Route>
-                }
-            </Switch>
-        </BrowserRouter>
+        <ConfigProvider locale={zhCN}>
+            <BrowserRouter>
+                <Home/>
+                <Switch>
+                    {
+                        routes.map(route =>
+                            // 使用exact来实现精准匹配
+                            <Route exact key={route.path} path={route.path}>
+                                <route.component/>
+                            </Route>
+                        )
+                        // <Route component={NotFound}></Route>
+                    }
+                </Switch>
+            </BrowserRouter>
+        </ConfigProvider>
     );
 }
 
