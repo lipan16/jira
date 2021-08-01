8aa81b3ecf0dbc2ad34253690a48bf693150d7a5
lipan16 <lipan16@lzu.edu.cn>
2021-08-01 21:35:54 +0800

    添加redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
+import store from './redux/store';
 import './main.less';
-import './utils/rem'
+import './utils/rem';
+
 
 ReactDOM.render(
+    //开启严格模式
     <React.StrictMode>
-        <App/>
+        {/*此处用provider包裹app，为了app的所有后代容器组件都能接收store*/}
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+// 只要store有改动就重新渲染整个页面（diff算法存在）
+// store.subscribe(() => {
+//     ReactDOM.render(
+//         <React.StrictMode>
+//             <App/>
+//         </React.StrictMode>,
+//         document.getElementById('root')
+//     );
+// });
