7cbad350ebc06388c2c863005f18f7bb02741de4
lipan16 <lipan16@lzu.edu.cn>
2021-08-06 20:37:47 +0800

    optimization

--- src/components/khjd/Cell/index.jsx
+++ src/components/khjd/Cell/index.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './index.less';
 
 export default function LpCell(props){
+
     return (
         <div className={["flex-inline", "lp-cell", props.className].join(" ")}>
             <div className="label">{props.label}</div>
             <div className="value">{props.value}</div>
+            {/*<div className='arrow'/>*/}
         </div>
     );
 }
--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,47 @@ import About from '@/components/test/about';
 import khjdDetail from '@/pages/khjd/detail/index';
 import taskHandle from '@/pages/khjd/taskHandle/index';
 import taskTransfer from '@/pages/khjd/taskTransfer/index';
+import HandleBasicInfo from '../pages/khjd/handleBasicInfo';
+import HandleRisk from '../pages/khjd/handleRisk';
+import HandleSurvey from '../pages/khjd/handleSurvey';
+import HandlePhoto from '../pages/khjd/handlePhoto';
 
 const routes = [
     {
-        path: RouterPath.index,
+        path     : RouterPath.index,
         component: Home
     },
     {
-        path: RouterPath.about,
+        path     : RouterPath.about,
         component: About
     },
     {
-        path: RouterPath.khjdDetail,
+        path     : RouterPath.khjdDetail,
         component: khjdDetail
     },
     {
-        path: RouterPath.taskHandle,
+        path     : RouterPath.taskHandle,
         component: taskHandle
     },
     {
-        path: RouterPath.taskTransfer,
+        path     : RouterPath.taskTransfer,
         component: taskTransfer
+    },
+    {
+        path     : RouterPath.handleBasicInfo,
+        component: HandleBasicInfo
+    },
+    {
+        path     : RouterPath.handleRisk,
+        component: HandleRisk
+    },
+    {
+        path     : RouterPath.handleSurvey,
+        component: HandleSurvey
+    },
+    {
+        path     : RouterPath.handlePhoto,
+        component: HandlePhoto
     }
 ];
 
