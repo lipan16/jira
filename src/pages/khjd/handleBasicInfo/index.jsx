25584ed6c8737452e6212fc39abd34f49818f60e
lipan16 <lipan16@lzu.edu.cn>
2021-08-07 21:25:27 +0800

    optimization

---
+++ src/pages/khjd/handleBasicInfo/index.jsx
@@ -0,0 +1,50 @@
+import React        from 'react';
+import {connect}    from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {Cell}       from 'zarm';
+
+import Navigation from '../../../components/khjd/Navigation';
+
+import './index.less';
+
+class HandleBasicInfo extends React.PureComponent {
+    gotoInfoDetail = (info) => {
+        console.log(info);
+        // this.props.history.push(info.path);
+    };
+    saveInfo       = () => {
+
+    };
+
+    render(){
+        console.log(this.props);
+        const {customerBasicInfo} = this.props;
+        const savedAble           = customerBasicInfo.every(item => item.status);
+        return (
+            <>
+                <Navigation title="客户基本信息"/>
+                <div className="basic-info flex-list">
+                    {
+                        customerBasicInfo.map((info, index) =>
+                            <Cell hasArrow key={index} title={info.name}
+                                  description={info.status ? '已保存' : '未保存'}
+                                  className={info.status ? 'info-saved' : 'info-unsaved'}
+                                  onClick={() => this.gotoInfoDetail(info)}
+                            />
+                        )
+                    }
+
+                    <div className={['unsaved', savedAble ? 'saved' : ''].join(' ')}
+                         onClick={() => savedAble && this.saveInfo()}>保存
+                    </div>
+                </div>
+
+            </>
+        );
+    }
+}
+
+export default withRouter(connect(
+    state => ({customerBasicInfo: state.khjdReducer.customerBasicInfo}),
+    {}
+)(HandleBasicInfo));
