2ce59edb080ad9d448bc50f7180b714b50948aa8
lipan16 <lipan16@lzu.edu.cn>
2021-08-07 21:19:24 +0800

    optimization

--- src/components/khjd/TaskWander/index.jsx
+++ src/components/khjd/TaskWander/index.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './index.less';
+import circleSvg from '../../../assets/circle.svg';
+import circleActiveSvg from '../../../assets/circle-active.svg';
 
 class TaskWander extends React.Component {
 
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        taskWanderList: PropTypes.arrayOf(PropTypes.object).isRequired
+    };
+
     render(){
+        const {title, taskWanderList} = this.props;
         return (
-            <></>
+            <>
+                <div className="title">{title}</div>
+                {
+                    taskWanderList.map((taskWander, index) => {
+                        return (
+                            <div className="flex-inline content" key={index}>
+                                <img src={taskWander.taskStatus === 'finished' ? circleActiveSvg : circleSvg}
+                                     alt=""/>
+                                <div className='task-person'>{taskWander.taskName}</div>
+                                <div className="strong"/>
+                                <div className='task-person'>{taskWander.taskPerson}</div>
+                                <div className="strong"/>
+                                <div className='task-person'>{taskWander.taskDuty}</div>
+                                <div className="strong"/>
+                                <div>{taskWander.taskTime}</div>
+                            </div>
+                        );
+                    })
+                }
+            </>
         );
     }
 }
