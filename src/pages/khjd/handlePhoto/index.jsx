1e1531163aec9d4aafd184fd731b4caf32403cea
lipan16 <lipan16@lzu.edu.cn>
2021-08-09 21:44:21 +0800

    optimization

---
+++ src/pages/khjd/handlePhoto/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.less';
+
+class HandlePhoto extends React.PureComponent {
+
+    render(){
+        return (
+            <></>
+        );
+    }
+}
+
+export default HandlePhoto;
