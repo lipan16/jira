93df7e1b4751b1b06e231475060097aa031bb47b
lipan16 <lipan16@lzu.edu.cn>
2021-08-09 21:45:43 +0800

    optimization

---
+++ src/pages/khjd/handleRisk/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.less';
+
+class HandleRisk extends React.PureComponent {
+
+    render(){
+        return (
+            <></>
+        );
+    }
+}
+
+export default HandleRisk;
