a741833e60a18a00f6989210bb0e73516c3c6646
lipan16 <lipan16@lzu.edu.cn>
2021-08-09 21:50:27 +0800

    khjd

---
+++ src/pages/khjd/handleSurvey/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.less';
+
+class HandleSurvey extends React.PureComponent {
+
+    render(){
+        return (
+            <></>
+        );
+    }
+}
+
+export default HandleSurvey;
