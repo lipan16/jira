6d3a97154908bc6c396d93bce81af11e6e6dbadd
lipan16 <lipan16@lzu.edu.cn>
2021-08-21 13:03:42 +0800

    khjd

--- src/components/khjd/Cell/index.jsx
+++ src/components/khjd/Cell/index.jsx
@@ -5,9 +5,9 @@ import './index.less';
 export default function LpCell(props){
 
     return (
-        <div className={["flex-inline", "lp-cell", props.className].join(" ")}>
+        <div className={['flex-inline', 'lp-cell', props.className].join(' ')}>
             <div className="label">{props.label}</div>
-            <div className="value">{props.value}</div>
+            <div className="value" onClick={props.onClick}>{props.value}</div>
             {/*<div className='arrow'/>*/}
         </div>
     );
--- src/utils/func.js
+++ src/utils/func.js
@@ -3,3 +3,24 @@ export function pageBack(param){
         history.go(param);
     };
 }
+
+export function dataMasking(data = '', type = 'default'){
+    switch(type){
+        case 'phone':
+            return data.replace(/(\d{3})\d*(\d{4})/, '$1****$2'); // 手机号脱敏
+        case 'name':
+            let name = '';
+            if(data.length <= 3){
+                name = '*' + data.substring(1, data.length);
+            }else if(data.length > 3 && data.length <= 6){
+                name = '**' + data.substring(2, data.length);
+            }else if(data.length > 6){
+                name = data.substring(0, 2) + '****' + data.substring(6, data.length);
+            }
+            return name;
+        case 'card':
+            return data.replace(/^(.{4})(?:\w+)(.{4})$/, '$1****$2');
+        default:
+            return data;
+    }
+}
